17954004c13171ad1acd5d88be0e9a2de26e52d5
Haomin Yu <yuhaomin1996@gmail.com>
2019-10-11 17:26:33 -0700

    Complete LCD GUI core design

--- src/TFT_Display.ino
+++ src/TFT_Display.ino
@@ -7,13 +7,13 @@
 static String clientName = "";
 static unsigned int clientAge    = -1;
 static unsigned int clientWeight = -1;
-static bool isMale = true;
+static unsigned int isMale = -1;
+static double BAC = -1;
 
 /**
  * Loads the screen prior to ID scan
  */
 void loadIDScanPromptScreen() {
-  tft.fillScreen(COLOR_BLACK);
   tft.setTextSize(TITLE_FONT);
   tft.setCursor(100, 0);
   tft.println("Welcome");
@@ -39,19 +39,36 @@ void loadIDScanWaitScreen() {
   tft.setTextSize(LABEL_FONT);
   tft.setCursor(15, 0);
   tft.println("Press button to confirm");
-  tft.drawRect(0, 16, camera.width(), camera.height() - 17, COLOR_YELLOW);
 }
 
 /**
- * 
+ * Displays the information the system currently has
  */
 void loadInformationScreen() {
-  
+  tft.setTextSize(LABEL_FONT);
+  tft.setCursor(70, 0);
+  tft.println("Your information");
+  tft.setCursor(0, 35);
+  tft.print("* Name: "); if(clientName == "") tft.println("[Unknown]");
+                         else tft.println(clientName);
+  tft.println();
+  tft.print("* Age : "); if(clientAge == -1) tft.println("[Unknown]");
+                         else tft.println(clientAge);
+  tft.println();
+  tft.print("* Weight: "); if(clientWeight == -1) tft.println("[Unknown]");
+                         else tft.println(clientWeight);
+  tft.println();
+  tft.print("* Gender: "); if(isMale == -1) tft.println("[Unknown]");
+                           else if(isMale == 0) tft.println("Female");
+                           else tft.println("Male");
+  tft.println("--------------------------\n");
+  tft.print("* BAC: "); if(BAC == -1) tft.println("[Unknown]");
+                        else tft.println(BAC);
 }
 
 /**
- * 
+ * Loads a prompt to breathe into the breathalyzer
  */
 void loadBreathalyzerPromptScreen() {
-  
+  // TODO - Undecided
 }
--- src/src.ino
+++ src/src.ino
@@ -24,5 +24,5 @@ void setup() {
 }
 
 void loop() {
-  loadIDScanWaitScreen();
+  
 }
