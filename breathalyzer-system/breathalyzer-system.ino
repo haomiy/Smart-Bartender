0a5cce0be9e3b55ab3dec0ec9790b1c855367cbf
Haomin Yu <yuhaomin1996@gmail.com>
2019-10-21 18:54:51 -0700

    Complete breathalyzer system

--- breathalyzer-system/breathalyzer-system.ino
+++ breathalyzer-system/breathalyzer-system.ino
@@ -4,21 +4,24 @@
  * data until requested
  * (Sensor MAXIMUM 0.05 - 500 ppm)
  */
+#include <ArduinoSort.h>
 #include "SystemConstants.h"
 
 void setup() {
   Serial.begin(BREATHALYZER_BAUD_RATE);
-
-  pinMode(2, OUTPUT);
-  digitalWrite(2, LOW);
-
-  for(int i = 0; i < 20; i++) {
-    int val = analogRead(A0);
-    Serial.println(val);
-    delay(100);
-  }
+  pinMode(BREATHALYZER_ENABLE_PIN, OUTPUT);
+  digitalWrite(BREATHALYZER_ENABLE_PIN, LOW);
 }
 
 void loop() {
-  
+  if(Serial.read() == SYSTEM_ENABLE_BYTE) {
+    for(int i = 0; i < MEASUREMENT_COUNT; i++) {
+      int val = analogRead(BREATHALYZER_DATA_PIN) * MAX_BREATHALYZER_VALUE / MAX_ANALOG_VALUE;
+      measurements[i] = val;
+      delay(100);
+    }
+    sortArrayReverse(measurements, MEASUREMENT_COUNT);
+    unsigned int result = (measurements[0] + measurements[1]) / 2;
+    sentToExternal(result);
+  }
 }
