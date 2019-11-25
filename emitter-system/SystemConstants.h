246ec96f1b6adb08ac91207a9c912a4958a03887
Haomin Yu <yuhaomin1996@gmail.com>
2019-11-25 15:06:27 -0800

    Replace dummy ports

--- emitter-system/SystemConstants.h
+++ emitter-system/SystemConstants.h
@@ -4,17 +4,14 @@
  */
 
 /* Pin assignments */
-const int rxControllerPin = 0;
-const int txControllerPin = 0;
+const int rxDispenserPin = 2;
+const int txDispenserPin = 3;
 
-const int rxDispenserPin = 0;
-const int txDispenserPin = 0;
+const int rxBreathalyzerPin = 4;
+const int txBreathalyzerPin = 5;
 
-const int rxBreathalyzerPin = 0;
-const int txBreathalyzerPin = 0;
-
-const int rxIDReaderPin = 0;
-const int txIDReaderPin = 0;
+const int rxIDReaderPin = 6;
+const int txIDReaderPin = 7;
 
 /* Identifiers */
 const byte SYSTEM_ID_READER    = 0x01;
